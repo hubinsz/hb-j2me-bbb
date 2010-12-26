79414a21023bf713dcd52d66ef99ce594de64bc6
ifoxbin <ifoxbin@170865b1-d06e-9bb6-dda3-35d0245d28c3>
2010-12-26 10:33:50 +0000

    this is j2me app for my dopod phone

---
+++ BoomBoomBoom/src/hb/brain/DataBaseRepository.java
@@ -0,0 +1,189 @@
+package hb.brain;
+
+public class DataBaseRepository {
+
+    public static int money = 0;
+    public static int amountBig = 0;
+    public static int amountSml = 0;
+    public static int amountEven = 0;
+    public static int amountOdd = 0;
+    public static String strAuto = "";
+    //
+    public static int anyThree = 0;
+    public static int anyThreeDiff = 0;
+    //
+    public static int intBig = 0;
+    public static int intSmall = 0;
+    public static int intBigDiff = 0;
+    public static int intSmallDiff = 0;
+    //
+    public static int intEven = 0;
+    public static int intOdd = 0;
+    public static int intEvenDiff = 0;
+    public static int intOddDiff = 0;
+    //
+    public static int intSingle1 = 0;
+    public static int intSingle2 = 0;
+    public static int intSingle3 = 0;
+    public static int intSingle4 = 0;
+    public static int intSingle5 = 0;
+    public static int intSingle6 = 0;
+    public static int intSingle1ns = 0;
+    public static int intSingle2ns = 0;
+    public static int intSingle3ns = 0;
+    public static int intSingle4ns = 0;
+    public static int intSingle5ns = 0;
+    public static int intSingle6ns = 0;
+    //
+    public static int intF3456 = 0;
+    public static int intF2356 = 0;
+    public static int intF2345 = 0;
+    public static int intF1234 = 0;
+    public static int intF3456ns = 0;
+    public static int intF2356ns = 0;
+    public static int intF2345ns = 0;
+    public static int intF1234ns = 0;
+    //
+    public static int anyDouble = 0;
+    public static int anyDoubleDiff = 0;
+    public static int intDouble1 = 0;
+    public static int intDouble2 = 0;
+    public static int intDouble3 = 0;
+    public static int intDouble4 = 0;
+    public static int intDouble5 = 0;
+    public static int intDouble6 = 0;
+    //
+    public static int anyTriple = 0;
+    public static int intTriple1 = 0;
+    public static int intTriple2 = 0;
+    public static int intTriple3 = 0;
+    public static int intTriple4 = 0;
+    public static int intTriple5 = 0;
+    public static int intTriple6 = 0;
+    //
+    public static int com12 = 0;
+    public static int com13 = 0;
+    public static int com14 = 0;
+    public static int com15 = 0;
+    public static int com16 = 0;
+    public static int com23 = 0;
+    public static int com24 = 0;
+    public static int com25 = 0;
+    public static int com26 = 0;
+    public static int com34 = 0;
+    public static int com35 = 0;
+    public static int com36 = 0;
+    public static int com45 = 0;
+    public static int com46 = 0;
+    public static int com56 = 0;
+    //
+    public static int sum4 = 0;
+    public static int sum5 = 0;
+    public static int sum6 = 0;
+    public static int sum7 = 0;
+    public static int sum8 = 0;
+    public static int sum9 = 0;
+    public static int sum10 = 0;
+    //
+    public static int sum11 = 0;
+    public static int sum12 = 0;
+    public static int sum13 = 0;
+    public static int sum14 = 0;
+    public static int sum15 = 0;
+    public static int sum16 = 0;
+    public static int sum17 = 0;
+
+    public static void clearSelf() {
+
+        strAuto= "";
+        anyThree = 0;
+        anyThreeDiff = 0;
+
+        intBig = 0;
+        intSmall = 0;
+        intBigDiff = 0;
+        intSmallDiff = 0;
+
+        intEven = 0;
+        intOdd = 0;
+        intEvenDiff = 0;
+        intOddDiff = 0;
+
+        intSingle1 = 0;
+        intSingle2 = 0;
+        intSingle3 = 0;
+        intSingle4 = 0;
+        intSingle5 = 0;
+        intSingle6 = 0;
+
+        intF3456 = 0;
+        intF2356 = 0;
+        intF2345 = 0;
+        intF1234 = 0;
+
+        anyDouble = 0;
+        anyDoubleDiff = 0;
+        intDouble1 = 0;
+        intDouble2 = 0;
+        intDouble3 = 0;
+        intDouble4 = 0;
+        intDouble5 = 0;
+        intDouble6 = 0;
+
+        anyTriple = 0;
+        intTriple1 = 0;
+        intTriple2 = 0;
+        intTriple3 = 0;
+        intTriple4 = 0;
+        intTriple5 = 0;
+        intTriple6 = 0;
+
+        com12 = 0;
+        com13 = 0;
+        com14 = 0;
+        com15 = 0;
+        com16 = 0;
+        com23 = 0;
+        com24 = 0;
+        com25 = 0;
+        com26 = 0;
+        com34 = 0;
+        com35 = 0;
+        com36 = 0;
+        com45 = 0;
+        com46 = 0;
+        com56 = 0;
+
+        sum4 = 0;
+        sum5 = 0;
+        sum6 = 0;
+        sum7 = 0;
+        sum8 = 0;
+        sum9 = 0;
+        sum10 = 0;
+
+        sum11 = 0;
+        sum12 = 0;
+        sum13 = 0;
+        sum14 = 0;
+        sum15 = 0;
+        sum16 = 0;
+        sum17 = 0;
+
+        intSingle1ns = 0;
+        intSingle2ns = 0;
+        intSingle3ns = 0;
+        intSingle4ns = 0;
+        intSingle5ns = 0;
+        intSingle6ns = 0;
+
+        intF3456ns = 0;
+        intF2356ns = 0;
+        intF2345ns = 0;
+        intF1234ns = 0;
+
+        money = 0;
+    }
+    //
+    public static StringBuffer stringBuffer = new StringBuffer();
+}
